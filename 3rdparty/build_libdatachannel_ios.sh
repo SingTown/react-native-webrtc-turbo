fc8aa026db127de07720d36dee89019ea9d6248c
kaizhi-singtown <kaizhi@singtown.com>
2025-08-17 18:51:19 +0800

    refactor: build libdatachannel ios script

--- 3rdparty/build_libdatachannel_ios.sh
+++ 3rdparty/build_libdatachannel_ios.sh
@@ -1,95 +1,60 @@
-# build MBEDTLS
+SDKS=("iphoneos" "iphonesimulator")
+
 (
     cd mbedtls
     python3 scripts/config.py set MBEDTLS_SSL_DTLS_SRTP
 )
 
-cmake -B build/mbedtls/ios-arm64 -G Xcode \
-    -DCMAKE_SYSTEM_NAME=iOS \
-    -DCMAKE_OSX_ARCHITECTURES=arm64 \
-    -DCMAKE_OSX_SYSROOT=iphoneos \
-    -DCMAKE_OSX_DEPLOYMENT_TARGET=12.0 \
-    -DCMAKE_INSTALL_PREFIX=build/mbedtls/ios-arm64/install \
-    -DENABLE_PROGRAMS=OFF -DENABLE_TESTING=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
-    mbedtls
-cmake --build build/mbedtls/ios-arm64 --config Release --target install
+for i in "${!SDKS[@]}"; do
+    SDK="${SDKS[$i]}"
+    
+    MBEDTLS_DIR="build/mbedtls/ios-$SDK-arm64"
+    cmake -B $MBEDTLS_DIR -G Xcode \
+        -DCMAKE_SYSTEM_NAME=iOS \
+        -DCMAKE_OSX_ARCHITECTURES=arm64 \
+        -DCMAKE_OSX_SYSROOT=$SDK \
+        -DCMAKE_OSX_DEPLOYMENT_TARGET=12.0 \
+        -DCMAKE_INSTALL_PREFIX=$MBEDTLS_DIR/install \
+        -DENABLE_PROGRAMS=OFF -DENABLE_TESTING=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
+        mbedtls
+    cmake --build $MBEDTLS_DIR --config Release --target install
 
-cmake -B build/mbedtls/ios-sim-arm64 -G Xcode \
-    -DCMAKE_SYSTEM_NAME=iOS \
-    -DCMAKE_OSX_ARCHITECTURES=arm64 \
-    -DCMAKE_OSX_SYSROOT=iphonesimulator \
-    -DCMAKE_OSX_DEPLOYMENT_TARGET=12.0 \
-    -DCMAKE_INSTALL_PREFIX=build/mbedtls/ios-sim-arm64/install \
-    -DENABLE_PROGRAMS=OFF -DENABLE_TESTING=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
-    mbedtls
-cmake --build build/mbedtls/ios-sim-arm64 --config Release --target install
+    LIBDATACHANNEL_DIR="build/libdatachannel/ios-$SDK-arm64"
+    cmake -B $LIBDATACHANNEL_DIR -G Xcode \
+        -DCMAKE_XCODE_ATTRIBUTE_CODE_SIGNING_ALLOWED=NO \
+        -DCMAKE_SYSTEM_NAME=iOS \
+        -DCMAKE_OSX_ARCHITECTURES=arm64 \
+        -DCMAKE_OSX_SYSROOT=$SDK \
+        -DCMAKE_OSX_DEPLOYMENT_TARGET=12.0 \
+        -DCMAKE_INSTALL_PREFIX=$LIBDATACHANNEL_DIR/install \
+        -DNO_TESTS=ON \
+        -DNO_EXAMPLES=ON \
+        -DBUILD_SHARED_LIBS=OFF \
+        -DBUILD_SHARED_DEPS_LIBS=OFF \
+        -DENABLE_WARNINGS_AS_ERRORS=OFF \
+        -DUSE_MBEDTLS=ON \
+        -DMbedTLS_INCLUDE_DIR=$MBEDTLS_DIR/install/include \
+        -DMbedTLS_LIBRARY=$MBEDTLS_DIR/install/lib/libmbedtls.a \
+        -DMbedCrypto_LIBRARY=$MBEDTLS_DIR/install/lib/libmbedcrypto.a \
+        -DMbedX509_LIBRARY=$MBEDTLS_DIR/install/lib/libmbedx509.a \
+        libdatachannel
+    cmake --build $LIBDATACHANNEL_DIR --config Release --target install
 
-# build libdatachannel
-cmake -B build/libdatachannel/ios-arm64 -G Xcode \
-    -DCMAKE_SYSTEM_NAME=iOS \
-    -DCMAKE_OSX_ARCHITECTURES=arm64 \
-    -DCMAKE_OSX_SYSROOT=iphoneos \
-    -DCMAKE_OSX_DEPLOYMENT_TARGET=12.0 \
-    -DCMAKE_INSTALL_PREFIX=build/libdatachannel/ios-arm64/install \
-    -DNO_TESTS=ON \
-    -DNO_EXAMPLES=ON \
-    -DBUILD_SHARED_LIBS=OFF \
-    -DBUILD_SHARED_DEPS_LIBS=OFF \
-    -DENABLE_WARNINGS_AS_ERRORS=OFF \
-    -DUSE_MBEDTLS=ON \
-    -DCMAKE_XCODE_ATTRIBUTE_CODE_SIGNING_ALLOWED=NO \
-    -DMbedTLS_INCLUDE_DIR=build/mbedtls/ios-arm64/install/include \
-    -DMbedTLS_LIBRARY=build/mbedtls/ios-arm64/install/lib/libmbedtls.a \
-    -DMbedCrypto_LIBRARY=build/mbedtls/ios-arm64/install/lib/libmbedcrypto.a \
-    -DMbedX509_LIBRARY=build/mbedtls/ios-arm64/install/lib/libmbedx509.a \
-    libdatachannel
-cmake --build build/libdatachannel/ios-arm64 --config Release --target install
-
-cmake -B build/libdatachannel/ios-sim-arm64 -G Xcode \
-    -DCMAKE_SYSTEM_NAME=iOS \
-    -DCMAKE_OSX_ARCHITECTURES=arm64 \
-    -DCMAKE_OSX_SYSROOT=iphonesimulator \
-    -DCMAKE_OSX_DEPLOYMENT_TARGET=12.0 \
-    -DCMAKE_INSTALL_PREFIX=build/libdatachannel/ios-sim-arm64/install \
-    -DNO_TESTS=ON \
-    -DNO_EXAMPLES=ON \
-    -DBUILD_SHARED_LIBS=OFF \
-    -DBUILD_SHARED_DEPS_LIBS=OFF \
-    -DENABLE_WARNINGS_AS_ERRORS=OFF \
-    -DUSE_MBEDTLS=ON \
-    -DCMAKE_XCODE_ATTRIBUTE_CODE_SIGNING_ALLOWED=NO \
-    -DMbedTLS_INCLUDE_DIR=build/mbedtls/ios-sim-arm64/install/include \
-    -DMbedTLS_LIBRARY=build/mbedtls/ios-sim-arm64/install/lib/libmbedtls.a \
-    -DMbedCrypto_LIBRARY=build/mbedtls/ios-sim-arm64/install/lib/libmbedcrypto.a \
-    -DMbedX509_LIBRARY=build/mbedtls/ios-sim-arm64/install/lib/libmbedx509.a \
-    libdatachannel
-cmake --build build/libdatachannel/ios-sim-arm64 --config Release --target install
-
-# 合并静态库
-libtool -static \
-  build/mbedtls/ios-arm64/install/lib/libmbedcrypto.a \
-  build/mbedtls/ios-arm64/install/lib/libmbedx509.a \
-  build/mbedtls/ios-arm64/install/lib/libmbedtls.a \
-  build/libdatachannel/ios-arm64/install/lib/libdatachannel.a \
-  build/libdatachannel/ios-arm64/install/lib/libjuice.a \
-  build/libdatachannel/ios-arm64/install/lib/libsrtp2.a \
-  build/libdatachannel/ios-arm64/install/lib/libusrsctp.a \
-  -o build/libdatachannel/ios-arm64/libdatachannel.a
-
-libtool -static \
-  build/mbedtls/ios-sim-arm64/install/lib/libmbedcrypto.a \
-  build/mbedtls/ios-sim-arm64/install/lib/libmbedx509.a \
-  build/mbedtls/ios-sim-arm64/install/lib/libmbedtls.a \
-  build/libdatachannel/ios-sim-arm64/install/lib/libdatachannel.a \
-  build/libdatachannel/ios-sim-arm64/install/lib/libjuice.a \
-  build/libdatachannel/ios-sim-arm64/install/lib/libsrtp2.a \
-  build/libdatachannel/ios-sim-arm64/install/lib/libusrsctp.a \
-  -o build/libdatachannel/ios-sim-arm64/libdatachannel.a
+    libtool -static \
+        $MBEDTLS_DIR/install/lib/libmbedcrypto.a \
+        $MBEDTLS_DIR/install/lib/libmbedx509.a \
+        $MBEDTLS_DIR/install/lib/libmbedtls.a \
+        $LIBDATACHANNEL_DIR/install/lib/libdatachannel.a \
+        $LIBDATACHANNEL_DIR/install/lib/libjuice.a \
+        $LIBDATACHANNEL_DIR/install/lib/libsrtp2.a \
+        $LIBDATACHANNEL_DIR/install/lib/libusrsctp.a \
+        -o $LIBDATACHANNEL_DIR/libdatachannel.a
+done
 
 rm -rf libdatachannel.xcframework
 xcodebuild -create-xcframework \
-  -library build/libdatachannel/ios-arm64/libdatachannel.a \
-  -headers build/libdatachannel/ios-arm64/install/include \
-  -library build/libdatachannel/ios-sim-arm64/libdatachannel.a \
-  -headers build/libdatachannel/ios-sim-arm64/install/include \
-  -output libdatachannel.xcframework
+    -library build/libdatachannel/ios-iphoneos-arm64/libdatachannel.a \
+    -headers build/libdatachannel/ios-iphoneos-arm64/install/include \
+    -library build/libdatachannel/ios-iphonesimulator-arm64/libdatachannel.a \
+    -headers build/libdatachannel/ios-iphonesimulator-arm64/install/include \
+    -output libdatachannel.xcframework
